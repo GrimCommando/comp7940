c03239014ca7dc83eb5c6b19daaaf889c4b66084
Carter Lim <21456291@life.hkbu.edu.hk>
2022-02-27 12:17:08 +0800

    Commit A.txt

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+def print_factor(x):
+    print(x)
+    
 def main():
-    print("Hello World")
+    # Find the all factors of x using a loop and the operator % 
+    # % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+    x = 52633
+    i=0
+    f=0
+    for i in range(x+1):
+    # your code here
+        if i == x:
+            break
+        i=i+1
+        f = x % i
+        print_factor(f)
 
 if __name__ == '__main__':
     main()
+    
