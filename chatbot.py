51e8b982d86dbeda92f2bb45441e23d8824919d5
Carter Lim <21456291@life.hkbu.edu.hk>
2022-02-28 20:23:56 +0800

    lab 4  commit

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,14 @@
 ## chatbot.py
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 # The messageHandler is used for all message updates
 import configparser
 import logging
+# Redis Database 
+import redis
 
+global redis
 
 def main():
     # Load your token and create an Updater for your Bot
@@ -12,6 +16,10 @@ def main():
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
+    
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
@@ -19,6 +27,13 @@ def main():
     # register a dispatcher to handle message: here we register an echo dispatcher
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    
+    # hello command
+    dispatcher.add_handler(CommandHandler("hello", hello_command))
 
     # To start the bot:
     updater.start_polling()
@@ -30,6 +45,34 @@ def echo(update, context):
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+
+def hello_command(update, context):
+    try:
+        reply_message = ('Hello ' + update.message.text[6:] +  '!')
+        logging.info("Update: " + str(update))
+        logging.info("context: " + str(context))
+        #update.message.reply_text('Hello ' + reply_message +  '!')
+        context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+    except:
+        update.message.reply_text('Usage: /hello <keyword>')
+
+def add(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
+    try: 
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]   # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg +  ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+
 
 if __name__ == '__main__':
     main()
